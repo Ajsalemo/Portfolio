4e5bec106fa951044c888e3ab332fa5025269e61
Anthony Salemo <ajssalemo@gmail.com>
2021-01-28 16:50:43 -0500

    added a response text/link box for the landing page

--- src/components/landing-box/landing-box.js
+++ src/components/landing-box/landing-box.js
@@ -1,5 +1,7 @@
 import React from "react"
 
 export default function LandingBox() {
-  return <div>Hello</div>
+  return (
+    <div className="bg-gray-700 absolute right-1/10 top-1/4 sm:right-1/4 w-4/5 h-1/2 sm:w-1/2 sm:h-1/2"></div>
+  )
 }
--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -1,14 +1,16 @@
+import LandingBox from "@components/landing-box/landing-box"
 import Img from "gatsby-image"
 import React from "react"
 
 export default function Landing({ data }) {
   return (
-    <div>
+    <div className="relative">
       <Img
         className="h-screen"
         fluid={data.file.childImageSharp.fluid}
         alt="A picture of the Brooklyn Bridge"
       />
+      <LandingBox />
     </div>
   )
 }
