d7aa918ab9043b218f38d1e817c4fd59c697830b
Anthony Salemo <ajssalemo@gmail.com>
2021-01-28 22:37:08 -0500

    fixed responsive resizing

--- src/components/landing-box/landing-box.js
+++ src/components/landing-box/landing-box.js
@@ -5,7 +5,7 @@ import React from "react"
 
 export default function LandingBox() {
   return (
-    <div className="bg-black bg-opacity-75 absolute right-1/10 top-1/4 sm:right-1/4 w-4/5 h-1/2 sm:w-1/2 sm:h-1/2 rounded-lg">
+    <div className="bg-black bg-opacity-75 absolute right-1/10 xs:right-1/4 md:right-1/4 top-1/4 w-4/5 h-1/2 xs:w-1/2 xs:h-1/2 sm:w-3/4 md:w-1/2 rounded-lg">
       <div className="text-white text-center flex flex-col h-3/4 justify-center">
         <h2 className="text-3xl sm:text-3xl lg:text-4xl font-pacifico pb-4">
           Hi, my name is
@@ -14,7 +14,7 @@ export default function LandingBox() {
           Anthony Salemo
         </h1>
       </div>
-      <div className="text-white flex justify-around bg-white bg-opacity-25 mx-2 sm:mx-12 p-4 text-sm sm:text-base">
+      <div className="text-white flex justify-around bg-white bg-opacity-25 mx-2 sm:mx-12 p-4 text-sm sm:text-base font-semibold">
         <div>
           <Link to="#">
             <FontAwesomeIcon icon={faLinkedin} size="2x" />
