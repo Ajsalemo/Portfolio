4d36f37d199fad6a8ed9742e9e77df84417c7100
Anthony Salemo <ajssalemo@gmail.com>
2021-01-29 22:49:09 -0500

    added dynamic links to the portfolio section for markdown pages

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,17 +1,30 @@
+import { Link } from "gatsby"
 import Img from "gatsby-image"
 import React from "react"
 
 export default function Portfolio({ data }) {
+  console.log(data.allFile.edges[0].node.childImageSharp.fluid)
   return (
     <div className="bg-black min-h-screen h-full">
-      <h2 className="text-white text-4xl sm:text-5xl lg:text-6xl font-pacifico py-12 text-center">Portfolio</h2>
+      <h2 className="text-white text-4xl sm:text-5xl lg:text-6xl font-pacifico py-12 text-center">
+        Portfolio
+      </h2>
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 ">
         {data.allFile.edges.map((src, i) => (
-          <Img
-            fluid={src.node.childImageSharp.fluid}
-            key={i}
-            className="h-fit"
-          />
+          <Link
+            // The .substr method removes the file extension
+            // The .replace method replaces the /static/<rid>/subfolder/ path leading to the image with an empty string
+            // To strip the prefixed file path and file extension to retrieve just the image name to use as a page link
+            to={src.node.childImageSharp.fluid.src
+              .substr(0, src.node.childImageSharp.fluid.src.lastIndexOf("."))
+              .replace(/^.*[\\\/]/, "")}
+          >
+            <Img
+              fluid={src.node.childImageSharp.fluid}
+              key={i}
+              className="h-fit"
+            />
+          </Link>
         ))}
       </div>
     </div>
