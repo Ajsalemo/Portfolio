3aa5d2b0b0d72c8e93324c66a0a081d01d65ad14
Anthony Salemo <ajssalemo@gmail.com>
2021-01-30 20:57:00 -0500

    added the SEO component to the projects markdown template

--- src/templates/projectsTemplate.js
+++ src/templates/projectsTemplate.js
@@ -1,7 +1,8 @@
 import Footer from "@components/footer/footer"
 import Navbar from "@components/navbar/navbar"
+import SEO from "@components/seo/seo"
 import { graphql } from "gatsby"
-import React from "react"
+import React, { Fragment } from "react"
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
@@ -9,18 +10,28 @@ export default function Template({
   const { markdownRemark } = data
   const { frontmatter, html } = markdownRemark
   return (
-    <div>
-      <Navbar />
-      <div className="min-h-screen h-full bg-blue-400 text-center px-12">
-        <h1 className="text-4xl sm:text-5xl lg:text-6xl font-pacifico text-center pt-12 text-white">
-          {frontmatter.title}
-        </h1>
-        <h3 className="text-white text-xs sm:text-sm pt-12 font-semibold">{frontmatter.descriptionMain}</h3>
-        <h4 className="text-white text-xs sm:text-sm pt-6">{frontmatter.descriptionSecondary}</h4>
-        <div dangerouslySetInnerHTML={{ __html: html }} />
+    <Fragment>
+      <SEO 
+        title={frontmatter.title}
+        description={frontmatter.descriptionMain}
+      />
+      <div>
+        <Navbar />
+        <div className="min-h-screen h-full bg-blue-400 text-center px-12">
+          <h1 className="text-4xl sm:text-5xl lg:text-6xl font-pacifico text-center pt-12 text-white">
+            {frontmatter.title}
+          </h1>
+          <h3 className="text-white text-xs sm:text-sm pt-12 font-semibold">
+            {frontmatter.descriptionMain}
+          </h3>
+          <h4 className="text-white text-xs sm:text-sm pt-6">
+            {frontmatter.descriptionSecondary}
+          </h4>
+          <div dangerouslySetInnerHTML={{ __html: html }} />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Fragment>
   )
 }
 
